01309f78bfb32fd9daa6bbfbec8c00e6725899f6
joe <joeprisaz@gmail.com>
2021-02-17 22:15:49 -0500

    added input for scale factor

--- myplotx.m
+++ myplotx.m
@@ -1,3 +1,9 @@
+function myplotx(scale)
+
+if nargin < 1
+    error("enter a scale factor")
+end
+
 % reset factory defaults
 set(groot, 'defaultLineLineWidth', get(groot, 'FactoryLineLineWidth'))
 set(groot, 'defaultFunctionlineLineWidth', get(groot, 'FactoryFunctionlineLineWidth'))
@@ -8,18 +14,20 @@ set(groot, 'defaultFigureUnits', get(groot, 'FactoryFigureUnits'))
 set(groot, 'defaultFigurePosition', get(groot, 'FactoryFigurePosition'))
 
 % lines
-set(groot, 'defaultLineLineWidth', 1.5 * 2)
-set(groot, 'defaultFunctionlineLineWidth', 1.5 * 2)
-set(groot, 'defaultImplicitFunctionlineLineWidth', 1.5 * 2)
-set(groot, 'defaultAxesLineWidth', 1.0 * 2)
+set(groot, 'defaultLineLineWidth', 1.5 * scale)
+set(groot, 'defaultFunctionlineLineWidth', 1.5 * scale)
+set(groot, 'defaultImplicitFunctionlineLineWidth', 1.5 * scale)
+set(groot, 'defaultAxesLineWidth', 1.0 * scale)
 
 % fonts
 set(groot, 'defaultAxesFontName', 'CMU Serif')
-set(groot, 'defaultAxesFontSize', 12 * 2)
+set(groot, 'defaultAxesFontSize', 12 * scale)
 
 % sizes
 set(groot, 'defaultFigureUnits', 'inches')
-set(groot, 'defaultFigurePosition', [0.5, 0.5, 7.0 * 2, 4.5 * 2])
+w = 7.0 * scale; h = 4.5 * scale;
+set(groot, 'defaultFigurePosition', [0.5, 0.5, w, h])
 
-fprintf("To override figure size: \n set(gcf, 'Position', [0.5, 0.5, 14.0, 9.0]) \n\n")
+fprintf(['To override figure size: \n set(gcf, ''Position'', [0.5, 0.5, ',num2str(w),', ',num2str(h),']) \n\n'])
 fprintf("To reduce white space: \n set(gca, 'LooseInset', get(gca,'TightInset') + 0.01) \n\n")
+end
