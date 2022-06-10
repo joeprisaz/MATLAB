4fc49b8a790c42b5fef5961bbc15e89f799ef221
Joe Prisaznuk <32753155+joeprisaz@users.noreply.github.com>
2022-06-10 11:53:35 -0400

    Simplified inputs, removed manual font settings

--- myplot.m
+++ myplot.m
@@ -1,58 +1,80 @@
-function myplot(scale,~,~)
+% myplot.m    Automatically set formatting options for figures/axes.
+%
+% Increases the default figure size and puts the origin at the lower left
+% corner. Adjusts the default line widths to match larger figure size. 
+% Position is slightly different on mac/windows to account for
+% taskbar size on windows (40 px). Text interpreter may be changed from
+% default tex to latex for common axis elements.
+%
+% The graphics root settings are changed such that all new figures during
+% the open matlab session will use the applied settings. Opening a new
+% matlab session will reset these changes. Also, calling myplot(0) will
+% reset to factory defaults.
 
-setInterpreter = true;
-sans = false;
+function myplot(scale,varargin)
+
+useLatex = false; % default to tex
+
+% for simplicity, these values are fixed but can be overwritten in a script
+% by changing the figure, axis, or plot settings.
+lineLineWd  = 1.5  * scale;
+axesLineWd  = 0.75 * scale;
+textFontSz  = 10   * scale;
+axesFontSz  = 12   * scale;
+figureWd    = 600  * scale;
+figureHt    = 400  * scale;
+
+% parse input
 if nargin < 1
-    scale = 1;
+    scale = 1; % set scale factor to 1
+elseif nargin == 1
+    % do nothing
 elseif nargin > 1
-    setInterpreter = false;
-    if nargin > 2
-        sans = true;
-    end
+    useLatex = varargin{2}; % set the interpreter manually
 end
 
+% reset factory defaults
 if scale == 0
-    % reset factory defaults
     set(groot, 'defaultLineLineWidth', get(groot, 'FactoryLineLineWidth'))
     set(groot, 'defaultFunctionlineLineWidth', get(groot, 'FactoryFunctionlineLineWidth'))
     set(groot, 'defaultImplicitFunctionlineLineWidth', get(groot, 'FactoryImplicitFunctionlineLineWidth'))
     set(groot, 'defaultAxesLineWidth', get(groot, 'FactoryAxesLineWidth'))
-    set(groot, 'defaultColorbarLineWidth', get(groot, 'factoryColorbarLineWidth'))
+    set(groot, 'defaultColorbarLineWidth', get(groot, 'FactoryColorbarLineWidth'))
     set(groot, 'defaultTextFontName', get(groot, 'FactoryTextFontName'))
     set(groot, 'defaultAxesFontName', get(groot, 'FactoryAxesFontName'))
-    set(groot, 'defaultTextInterpreter', get(groot, 'factoryTextInterpreter'))
-    set(groot, 'defaultAxesTickLabelInterpreter', get(groot, 'factoryAxesTickLabelInterpreter'))
-    set(groot, 'defaultColorbarTickLabelInterpreter', get(groot, 'factoryColorbarTickLabelInterpreter'))
-    set(groot, 'defaultLegendInterpreter', get(groot, 'factoryLegendInterpreter'))
+    set(groot, 'defaultTextInterpreter', get(groot, 'FactoryTextInterpreter'))
+    set(groot, 'defaultAxesTickLabelInterpreter', get(groot, 'FactoryAxesTickLabelInterpreter'))
+    set(groot, 'defaultColorbarTickLabelInterpreter', get(groot, 'FactoryColorbarTickLabelInterpreter'))
+    set(groot, 'defaultLegendInterpreter', get(groot, 'FactoryLegendInterpreter'))
     set(groot, 'defaultTextFontSize', get(groot, 'FactoryTextFontSize'))
     set(groot, 'defaultAxesFontSize', get(groot, 'FactoryAxesFontSize'))
-    set(groot, 'defaultColorbarFontSize', get(groot, 'factoryColorbarFontSize'))
+    set(groot, 'defaultColorbarFontSize', get(groot, 'FactoryColorbarFontSize'))
     set(groot, 'defaultFigurePosition', get(groot, 'FactoryFigurePosition'))
     return
 end
 
-% lines
-set(groot, 'defaultLineLineWidth', 1.5 * scale)
-set(groot, 'defaultFunctionlineLineWidth', 1.5 * scale)
-set(groot, 'defaultImplicitFunctionlineLineWidth', 1.5 * scale)
-set(groot, 'defaultAxesLineWidth', 0.75 * scale)
-set(groot, 'defaultColorbarLineWidth', 0.5 * scale)
+% line width
+set(groot, 'defaultLineLineWidth', lineLineWd)
+set(groot, 'defaultFunctionlineLineWidth', lineLineWd)
+set(groot, 'defaultImplicitFunctionlineLineWidth', lineLineWd)
+set(groot, 'defaultAxesLineWidth', axesLineWd)
+set(groot, 'defaultColorbarLineWidth', axesLineWd)
 
+% font size
+set(groot, 'defaultTextFontSize', textFontSz)
+set(groot, 'defaultColorbarFontSize', textFontSz)
+set(groot, 'defaultAxesFontSize', axesFontSz)
 
-% fonts
-if sans == true
-    set(groot, 'defaultTextFontName', 'DejaVu Sans')
-    set(groot, 'defaultAxesFontName', 'DejaVu Sans')
+% figure size
+if ismac
+    x = 20; y = 20;
 else
-    set(groot, 'defaultTextFontName', 'CMU Serif')
-    set(groot, 'defaultAxesFontName', 'CMU Serif')
+    x = 11; y = 51;
 end
-set(groot, 'defaultTextFontSize', 10 * scale)
-set(groot, 'defaultColorbarFontSize', 9 * scale)
-set(groot, 'defaultAxesFontSize', 12 * scale)
+set(groot, 'defaultFigurePosition', [x, y, figureWd, figureHt])
 
-% interpreter
-if setInterpreter == true
+% set interpreter
+if useLatex == true
     set(groot, 'defaultTextInterpreter', 'latex')
     set(groot, 'defaultAxesTickLabelInterpreter', 'latex')
     set(groot, 'defaultColorbarTickLabelInterpreter', 'latex')
@@ -64,35 +86,13 @@ else
     set(groot, 'defaultLegendInterpreter', get(groot, 'factoryLegendInterpreter'))
 end
 
-% sizes
-w = 600 * scale; h = 400 * scale;
-if ismac; x = 20; y = 20; else; x = 11; y = 51; end
-set(groot, 'defaultFigurePosition', [x, y, w, h])
-
 % command window outputs
-fprintf('Arguments\n')
-fprintf('0'); fprintf(' - scale factor set to 1\n')
-fprintf('1'); fprintf(' - scale factor (interpreter set to latex)\n')
-fprintf('2'); fprintf(' - scale factor + set interpreter to default (tex)\n')
-fprintf('3'); fprintf(' - sans font\n')
-fprintf('myplot(0) for factory defaults\n\n')
+fprintf('\nScaling\n')
+fprintf('Line, Axes linewidth = %.2f, %.2f\n',lineLineWd, axesLineWd)
+fprintf('Axes, Text font size = %.2f, %.2f\n',axesFontSz, textFontSz)
 
-fprintf('Scaling\n')
-fprintf('Line linewidth = %.2f\n',1.5*scale)
-fprintf('Axes linewidth = %.2f\n',0.75*scale)
-if setInterpreter == true
-    fprintf('Text font size = %.2f (latex)\n',10*scale)
-    fprintf('Axes font size = %.2f (latex)\n\n',12*scale)
-else
-    fprintf('Text font size = %.2f (tex)\n',10*scale)
-    fprintf('Axes font size = %.2f (tex)\n\n',12*scale)
-end
-
-fprintf('Commonly used commands\n')
-fprintf('Override figure size\n')
+fprintf('\nOverride figure size\n')
 fprintf('set(gcf, ''Position'', [%.f, %.f, %.f, %.f])\n\n',x,y,w,h)
-fprintf('Override opengl renderer\n')
-fprintf('set(gcf, ''Renderer'', ''Painters'')\n\n')
 fprintf('Export pdf\n')
 fprintf("exportgraphics(gcf,'filename.pdf','ContentType','vector')\n\n")
 fprintf('Export png\n')
